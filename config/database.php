4f721e002318b4f21eca2bf2e58bf4ba04cfdcaa
aaron dalao <aaron.dalao@gmail.com>
2022-07-01 15:51:41 +0800

    editing config/database.php to fix mongodb connection issues

--- config/database.php
+++ config/database.php
@@ -102,7 +102,7 @@ return [
             'database' => env('MONGO_DATABASE', 'forge'),
             'username' => env('MONGO_USERNAME', 'forge'),
             'password' => env('MONGO_PASSWORD', ''),
-//            'dsn' => env('MONGO_URI', 'mongodb+srv://username:password@<atlas-cluster-uri>/myappdb?retryWrites=true&w=majority'),
+            'dsn' => env('MONGO_URI', 'mongodb+srv://username:password@<atlas-cluster-uri>/myappdb?retryWrites=true&w=majority'),
             'options' => [
                 // here you can pass more settings to the Mongo Driver Manager
                 // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
